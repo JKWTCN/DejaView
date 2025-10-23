effa1ae5da41dcbb000d65499e03664610110e82
JKWTCN <jkwtcn@icloud.com>
2025-10-23 20:59:52 +0800

    feat(ui): 添加主窗口工具栏和菜单栏
    - 在主窗口中添加工具栏，包含多个操作按钮
    - 添加文件、编辑、视图、搜索和帮助菜单
    - 添加资源文件 res.qrc 以管理图标资源
    - 更新主窗口 UI 布局，添加图形视图和列表视图
    - 添加多语言翻译条目，支持中英文界面
    - 添加多个图标资源文件，用于工具栏按钮显示

--- DejaView_zh_CN.ts
+++ DejaView_zh_CN.ts
@@ -7,8 +7,38 @@
         <location filename="mainwindow.ui" line="14"/>
         <location filename="ui/mainwindow.ui" line="14"/>
         <location filename="build/DejaView_autogen/include/ui_mainwindow.h" line="51"/>
-        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/DejaView_autogen/include/ui_mainwindow.h" line="51"/>
+        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/DejaView_autogen/include/ui_mainwindow.h" line="120"/>
         <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="60"/>
+        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/DejaView_autogen/include/ui_mainwindow.h" line="121"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="65"/>
+        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/DejaView_autogen/include/ui_mainwindow.h" line="122"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/DejaView_autogen/include/ui_mainwindow.h" line="123"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/DejaView_autogen/include/ui_mainwindow.h" line="124"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <location filename="build/Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/DejaView_autogen/include/ui_mainwindow.h" line="125"/>
+        <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,41 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include <qtoolbar.h>
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QToolBar *mainBar = new QToolBar(this);
+    this->addToolBar(mainBar);
+    QAction *cfgLoadBtn = new QAction(QIcon(":/res/cfg_load.png"), "Load Search Configuration", this);
+    mainBar->addAction(cfgLoadBtn);
+    QAction *cfgSaveBtn = new QAction(QIcon(":/res/save.png"), "Save Search Configuration", this);
+    mainBar->addAction(cfgSaveBtn);
+    QAction *startSearchBtn = new QAction(QIcon(":/res/start.png"), "Start Search", this);
+    mainBar->addAction(startSearchBtn);
+    QAction *refreshResultBtn = new QAction(QIcon(":/res/refresh.png"), "Refresh Search Result", this);
+    mainBar->addAction(refreshResultBtn);
+    QAction *openFolderBtn = new QAction(QIcon(":/res/filefolder.png"), "Open Folder", this);
+    mainBar->addAction(openFolderBtn);
+    QAction *lastStepBtn = new QAction(QIcon(":/res/left.png"), "Last Step", this);
+    mainBar->addAction(lastStepBtn);
+    QAction *nextStepBtn = new QAction(QIcon(":/res/right.png"), "Next Step", this);
+    mainBar->addAction(nextStepBtn);
+    QAction *warningBtn = new QAction(QIcon(":/res/warning.png"), "Warning", this);
+    mainBar->addAction(warningBtn);
+    QAction *autoBtn = new QAction(QIcon(":/res/auto.png"), "Auto", this);
+    mainBar->addAction(autoBtn);
+    QAction *deleteTopBtn = new QAction(QIcon(":/res/delete_top.png"), "Delete Top", this);
+    mainBar->addAction(deleteTopBtn);
+    QAction *deleteBottomBtn = new QAction(QIcon(":/res/delete_bottom.png"), "Delete Bottom", this);
+    mainBar->addAction(deleteBottomBtn);
+    QAction *deleteAllBtn = new QAction(QIcon(":/res/delete_all.png"), "Delete All", this);
+    mainBar->addAction(deleteAllBtn);
+    QAction *TopToBottomBtn = new QAction(QIcon(":/res/top_to_bottom.png"), "Replace the image below with the one above.", this);
+    mainBar->addAction(TopToBottomBtn);
+    QAction *BottomToTopBtn = new QAction(QIcon(":/res/bottom_to_top.png"), "Replace the image above with the one below.", this);
+    mainBar->addAction(BottomToTopBtn);
+    // mainBar->setIconSize(QSize(40, 40));
 }
 
 MainWindow::~MainWindow()
